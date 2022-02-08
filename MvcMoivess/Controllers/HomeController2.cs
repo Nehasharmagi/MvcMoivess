0c14423a9f3d6dd61f708b3abde2a09fe2729d3f
Neha Sharma <NS232@myscc.ca>
2022-02-08 10:35:46 -0500

    Add project files.

---
+++ MvcMoivess/Controllers/HomeController2.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMoivess.Controllers
+{
+    public class HomeController2 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
